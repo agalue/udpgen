f694725f4c69dbf6954f8d1ab17fcc937c1c4501
Alejandro Galue <agalue@opennms.org>
2021-06-15 15:05:32 -0400

    Parametrizing SNMP Trap and Syslog content
    Added CLI flags to customize the content of the SNMP Traps and Syslog
    messages.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"log"
+	"log/syslog"
 	"os"
 	"os/signal"
 
@@ -15,15 +16,30 @@ func main() {
 
 	var payload string
 	cfg := new(generator.Config)
+
 	flag.StringVar(&payload, "x", "syslog", "Type of payload: snmp, syslog, netflow5, netflow9")
-	flag.StringVar(&cfg.Host, "h", "127.0.0.1", "Target host / IP address")
+	flag.StringVar(&cfg.Host, "h", "127.0.0.1", "Target Hostname or IP address")
 	flag.IntVar(&cfg.Port, "p", 0, "Target port (default depends on mode)")
 	flag.IntVar(&cfg.PacketsPerSecond, "r", 10000, "Number of packets per second to generate")
 	flag.IntVar(&cfg.Workers, "w", 1, "Number of workers (concurrent go-routines)")
+
+	flag.StringVar(&cfg.TrapVersion, "trap-version", "v1", "SNMP Trap Version: v1 or v2c")
+	flag.StringVar(&cfg.TrapSource, "trap-host", "127.0.0.1", "IP Address or Hostname of the Trap Sender")
+	flag.StringVar(&cfg.TrapID, "trap-id", ".1.3.6.1.1.6.3.1.1.5", "SNMPv1 Trap Enterprise or SNMPv2c Trap ID")
+	flag.IntVar(&cfg.TrapSpecific, "trap-specific", 1, "SNMPv1 Trap Specific")
+	flag.IntVar(&cfg.TrapGeneric, "trap-generic", 6, "SNMPv1 Trap Generic")
+	flag.Var(&cfg.TrapVarbinds, "trap-varbind", "An SNMP trap varbind (can be used multiple times)\nfor instance: .1.3.6.1.6.3.1.1.5.1::ABC (octet-string assume)")
+
+	flag.IntVar(&cfg.SyslogFacility, "syslog-facility", int(syslog.LOG_LOCAL7), "Syslog Facility, from /usr/include/sys/syslog.h")
+	flag.StringVar(&cfg.SyslogMessage, "syslog-message", "%%SEC-6-IPACCESSLOGP: list in110 denied tcp 10.99.99.1(63923) -> 10.98.98.1(1521), 1 packet", "Syslog Message")
+
 	flag.Parse()
 
 	if cfg.PacketsPerSecond <= 0 {
 		log.Fatalln("Packet rate cannot be zero.")
+	}
+	if cfg.TrapVarbinds == nil {
+		cfg.TrapVarbinds = append(cfg.TrapVarbinds, ".1.3.6.1.6.3.1.1.5.1::ABC")
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
